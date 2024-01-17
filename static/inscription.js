5b6cdad1e6f8c5dbfdc35fd690bae5dd52636f8c
fantasy2345 <renokamoto325@gmail.com>
2024-01-18 07:35:52 +0900

    Add: 2023.04.28 atanaka0313: issue: fixes for copy text to clipboard

--- static/inscription.js
+++ static/inscription.js
@@ -47,11 +47,43 @@ if (sat_block) {
 const copyIcons = document.getElementsByClassName("copy-icon");
 for (const copyIcon of copyIcons) {
   copyIcon.addEventListener('click', () => {
-    if (navigator.clipboard) {
-        navigator.clipboard.writeText(copyIcon.dataset.copy);
-      } else {
-        console.log("Failed to copy to clipboard");
-      }
+    fallbackCopyTextToClipboard(copyIcon.dataset.copy);
+  });
+}
+
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Fallback: Copying text command was ' + msg);
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err);
+  }
+
+  document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(function() {
+    console.log('Async: Copying to clipboard was successful!');
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err);
   });
 }
 
